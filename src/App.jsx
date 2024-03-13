1d0b0e099514066bb58c30b228f5810aecbb27f7
Dinesh <Dineshchauhan416pd@gmail.com>
2024-03-13 18:29:02 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="w-screen m-0 flex flex-row justify-center p-0 h-screen bg-[#041a2f]">
+      <div className="w-[1080px] bg-[#e7099644] h-screen">
+        <h1 className="text-[48px] text-center text-blue-400">Hello from Redux</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
