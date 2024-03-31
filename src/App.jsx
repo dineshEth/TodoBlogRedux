e0d72aea3a9fc5307fadcb27f2b85a4d4f39cac3
Dinesh <Dineshchauhan416pd@gmail.com>
2024-03-31 07:07:05 +0530

    update in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@
 function App() {
 
   return (
-    <div className="w-screen m-0 flex flex-row justify-center p-0 h-screen bg-[#041a2f]">
-      <div className="w-[1080px] bg-[#e7099644] h-screen">
-        <h1 className="text-[48px] text-center text-blue-400">Hello from Redux</h1>
+    <div className="w-full m-0 flex flex-row justify-center p-0 min-h-screen bg-[#041a2f]">
+      <div className="w-[1080px] mx-auto">
+        <h1 className="text-[48px]  text-orange-600">Todo</h1>
       </div>
     </div>
   )
